2339633a4e08d6369425099fdb5e276a0671d0e7
bcachain <bcachain@gmail.com>
2018-10-18 11:19:07 +0800

    add build

---
+++ build/precache-manifest.8da58368e39e626b0f625af7b2e2bc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "36e3d08daeb017db23a1",
+    "url": "./static/js/main.36e3d08d.chunk.js"
+  },
+  {
+    "revision": "04d6e7c128b891814ba0",
+    "url": "./static/js/1.04d6e7c1.chunk.js"
+  },
+  {
+    "revision": "36e3d08daeb017db23a1",
+    "url": "./static/css/main.16037928.chunk.css"
+  },
+  {
+    "revision": "04d6e7c128b891814ba0",
+    "url": "./static/css/1.b06278cd.chunk.css"
+  },
+  {
+    "revision": "45e234cde483aaa2be83206d1a5e5093",
+    "url": "./index.html"
+  }
+];
